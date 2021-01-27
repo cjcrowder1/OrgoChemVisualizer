a40e425cff212210dea0fb11ca52c774e6f704cc
Patrick Eastham <peastham@math.fsu.edu>
2021-01-27 14:05:53 -0500

    refactored code

--- orgochemvisualizer/__main__.py
+++ orgochemvisualizer/__main__.py
@@ -7,64 +7,60 @@ It is used to visualize simple organic chemistry reactions.
 import sys
 import os
 import numpy as np
-import pyqtgraph as pg
-from PyQt5 import QtGui, QtCore, QtWidgets, uic
-import mymath
 
-class orgochemvisualizer(QtWidgets.QMainWindow):
+import pyqtgraph as pg
+from PyQt5 import QtGui, QtWidgets
+
+import chemanim as ca
+
+
+class AnimationViewer(pg.GraphicsLayoutWidget):
+    """ main class for viewing orgo chemicals """
+    def __init__(self):
+        super(AnimationViewer, self).__init__()
+        v = self.addViewBox()
+        v.setAspectLocked()
+        v.setLimits(xMin=-10, xMax=10, yMin=-10, yMax=10)
+        v.setXRange(-10, 10)
+        v.setYRange(-10, 10)
+
+        mol1 = ca.H2O()
+        mol2 = ca.CO2()
+        
+        v.addItem(mol1)
+        v.addItem(mol2)
+
+
+class MainWindow(QtWidgets.QMainWindow):
     """ main class for OrgoChemVisualizer """
     def __init__(self):
-        super(orgochemvisualizer, self).__init__()
+        super(MainWindow, self).__init__()
+
         self.init_ui()
 
     def init_ui(self):
-        uiPath = os.path.join("orgochemvisualizer","ui","orgochemvisualizer.ui")
-        self.ui = uic.loadUi(uiPath)
+        self.setWindowTitle('OrgoChemVisualizer')
+        self.resize(600, 600)
 
-        self.ui.setWindowTitle('OrgoChemVisualizer')
-        #self.ui.setWindowIcon(QtGui.QIcon('logo.png'))
+        self.av = AnimationViewer()
 
-        # this is just a test that mymath was imported as expected
-        a = mymath.my_square_root(324.0)
+        main_layout = QtWidgets.QHBoxLayout()
+        main_layout.addWidget(self.av)
 
-        self.scene = QtGui.QGraphicsScene()
-        self.ui.mywidget.setScene(self.scene)
-
-        ellipse1 = QtGui.QGraphicsEllipseItem(0,0,100,10)
-        ellipse1.setBrush(QtGui.QBrush(QtCore.Qt.red, style = QtCore.Qt.SolidPattern))        
-        self.scene.addItem(ellipse1) # add ellipse!
-        
-        # there's gotta be a better way to do this, but I don't have it now.
-        self.ui.checkBox1.setChecked(True)
-        self.ui.checkBox1.stateChanged.connect(lambda:self.checkbox_state(self.ui.checkBox1))
-        self.ui.checkBox2.setChecked(True)
-        self.ui.checkBox2.stateChanged.connect(lambda:self.checkbox_state(self.ui.checkBox2))
-        self.ui.checkBox3.setChecked(True)
-        self.ui.checkBox3.stateChanged.connect(lambda:self.checkbox_state(self.ui.checkBox3))
-        self.ui.checkBox4.setChecked(True)
-        self.ui.checkBox4.stateChanged.connect(lambda:self.checkbox_state(self.ui.checkBox4))
-        self.ui.checkBox5.setChecked(True)
-        self.ui.checkBox5.stateChanged.connect(lambda:self.checkbox_state(self.ui.checkBox5))
-        # this doesn't work as expected, but it is the closest alternative
-        #self.checkboxes = (self.ui.checkboxes.itemAt(i).widget() for i in range(self.ui.checkboxes.count())) 
-        #for item in self.checkboxes:
-        #    item.setChecked(True)
-        #    item.stateChanged.connect(lambda:self.checkbox_state(item))
-
-        self.ui.show()
-
-    def checkbox_state(self,b):
-        if b.isChecked():
-            print(b.text()+" is selected")
-        else:
-            print(b.text()+" is deselected")
+        main_widget = QtWidgets.QWidget()
+        main_widget.setLayout(main_layout)
+        self.setCentralWidget(main_widget)
 
 
 def main():
     app = QtGui.QApplication(sys.argv)
     app.setApplicationName('OrgoChemVisualizer')
-    orgochemvisualizer()
+
+    main = MainWindow()
+    main.show()
+
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
---
+++ orgochemvisualizer/chemanim.py
@@ -0,0 +1,156 @@
+"""
+This module contains classes to draw chemicals.
+
+It will first use a "displayed formula", but then eventually it would be
+nice to add in skeletal formula for more typical orgo representations
+"""
+
+import numpy as np
+import pyqtgraph as pg
+from PyQt5 import QtCore
+
+
+class Chemical(pg.GraphItem):
+    """ Class for drawing chemicals. Initial position is a
+    2-item numpy 1D-array """
+
+    def __init__(self):
+        self.dragPoint = None
+        self.dragOffset = None
+        self.textItems = []
+
+        # for some reason this super init has to be below
+        # the above 3 initializations
+        super(Chemical, self).__init__()
+
+        self.scatter.sigClicked.connect(self.clicked)
+
+    def setData(self, **kwds):
+        self.text = kwds.pop('text', [])
+        self.data = kwds
+        if 'pos' in self.data:
+            self.npts = self.data['pos'].shape[0]
+            self.data['data'] = np.empty(self.npts, dtype=[('index', int)])
+            self.data['data']['index'] = np.arange(self.npts)
+        self.setTexts(self.text)
+        self.updateGraph()
+
+    def setTexts(self, text):
+        for i in self.textItems:
+            i.scene().removeItem(i)
+        self.textItems = []
+        for t in text:
+            item = pg.TextItem(t)
+            self.textItems.append(item)
+            item.setParentItem(self)
+
+    def updateGraph(self):
+        pg.GraphItem.setData(self, **self.data)
+        for i, item in enumerate(self.textItems):
+            item.setPos(*self.data['pos'][i])
+
+    def mouseDragEvent(self, ev):
+        if ev.button() != QtCore.Qt.LeftButton:
+            ev.ignore()
+            return
+
+        if ev.isStart():
+            # We are already one step into the drag.
+            # Find the point(s) at the mouse cursor when the button was first
+            # pressed:
+            pos = ev.buttonDownPos()
+            pts = self.scatter.pointsAt(pos)
+            if len(pts) == 0:
+                ev.ignore()
+                return
+            for ti in range(self.npts):
+                self.dragPoint = pts[0]
+
+            ind = pts[0].data()[0]
+            self.dragOffset = self.data['pos'][ind] - pos
+
+        elif ev.isFinish():
+            self.dragPoint = None
+            return
+        else:
+            if self.dragPoint is None:
+                ev.ignore()
+                return
+
+        ind = self.dragPoint.data()[0]
+
+        diff = np.zeros((self.npts, 2))
+        for ti in range(self.npts):
+            diff[ti, :] = self.data['pos'][ti] - self.data['pos'][ind]
+
+        for ti in range(self.npts):
+            self.data['pos'][ti] = diff[ti, :] + ev.pos() + self.dragOffset
+
+        self.updateGraph()
+        ev.accept()
+
+    def clicked(self, pts):
+        print("clicked: %s" % pts)
+
+
+def LinInterp(p, q, t):
+    """ Linearly interpolates between two points p and q to a point r(t),
+    where t is between 0 and 1.
+
+    Note that r(0)=p, and r(1)=q."""
+    r = t*p + (1 - t)*q
+    return r
+
+
+class H2O(Chemical):
+    def __init__(self):
+        super(H2O, self).__init__()
+
+        pos = self.get_positions()
+        adj = self.get_edges()
+        texts = self.get_texts()
+
+        self.setData(pos=pos, adj=adj, pxMode=True, text=texts)
+
+    def get_positions(self):
+        return np.array([
+                        [-0.5, -0.5],
+                        [0.5, -0.5],
+                        [0, 0],
+                        ], dtype=float)
+
+    def get_edges(self):
+        return np.array([
+                        [0, 2],
+                        [1, 2],
+                        ])
+
+    def get_texts(self):
+        return ["H", "H", "O"]
+
+
+class CO2(Chemical):
+    def __init__(self):
+        super(CO2, self).__init__()
+
+        pos = self.get_positions()
+        adj = self.get_edges()
+        texts = self.get_texts()
+
+        self.setData(pos=pos, adj=adj, pxMode=True, text=texts)
+
+    def get_positions(self):
+        return np.array([
+                        [-1, 0],
+                        [0, 0],
+                        [1, 0],
+                        ], dtype=float)
+
+    def get_edges(self):
+        return np.array([
+                        [0, 2],
+                        [1, 2],
+                        ])
+
+    def get_texts(self):
+        return ["O", "C", "O"]
