0a21b1c16fd664661db102f19b887fb2f84aa888
Patrick Eastham <peastham@math.fsu.edu>
2021-02-25 21:35:56 -0500

    fixed missing functions

--- test/test_chemanim.py
+++ test/test_chemanim.py
@@ -10,13 +10,6 @@ class ExampleTest(unittest.TestCase):
         self.assertEqual(4.31, 4.31)
         self.assertEqual("this is an example test", "this is an example test")
 
-    def test_my_square_root(self):
-        self.assertEqual(18.0, my_square_root(324.0))
-        self.assertEqual(25.4, my_square_root(645.16))
-        self.assertEqual(50.332, my_square_root(2533.310224))
-        self.assertEqual(0.0, my_square_root(0.0))
-        self.assertEqual(-1, my_square_root(-22.0))
-
 
 if __name__ == '__main__':
     unittest.main()
